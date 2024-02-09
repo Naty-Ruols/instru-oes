46acd10c2fb7a82f4074ab7f5aae81cefd90dd52
Natalia Cerqueira <153464979+Naty-Ruols@users.noreply.github.com>
2024-02-08 22:59:07 -0300

    Add files via upload
    Nesse exemplo utilizei as instruções if e swith, no caso ao escolher uma das bebibas disponiveis ele retorna uma mensagem informando o valor, caso contrario aparece uma mensagem dizendo produto invalido e os produtos disponiveis para escolha

---
+++ exemplo.js
@@ -0,0 +1,13 @@
+switch("suco"){
+    case"cha":
+    console.log("chá por R$5,00");
+        break;
+    case"leite":
+    console.log("leite por R$8,00");
+        break;
+        case"cafe":
+    console.log("cafe por R$6,00");
+        break;
+        default:
+            console.log("Produto não encontrado por favor escolha as bebibas disponiveis temos cafe, leite e cha")
+}
---
+++ instrucao.js
@@ -0,0 +1,7 @@
+var pais = "brasil";
+
+if(pais != "Brasil"){
+    console.log("Voce é Estrangeiro");
+}else{
+    console.log("Voce é brasileiro")
+}
